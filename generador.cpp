f2d1e6036af3e20f9b8fad252163b79c37199412
Andreas Fast <andreas.fast@moove-it.com>
2012-06-24 15:54:49 -0300

    reorg de codigo

--- generador.cpp
+++ generador.cpp
@@ -168,6 +168,281 @@ void generar_compstmt(list<ast*> *stmt_list, list<Instruccion*> *codigo){
   cout << "fin stmt_list" << endl;
 }
 
+
+
+void generar_if(ast* nodo, std::list<Instruccion*> *codigo){}
+void generar_mul(ast* nodo, std::list<Instruccion*> *codigo){
+  RNumeric* arg2, *arg3, *arg1;
+  if (nodo_hoja(nodo->h1)){ // no preciso variable temporal
+    arg2 = get_numeric_node(nodo->h1);
+  } else {
+    decidir_nodo(nodo->h1, codigo); //ultima operacion debe ser numerica y guardar el resultado en arg1
+    arg2 = (RNumeric*)codigo->back()->arg1;
+  }
+  if (nodo_hoja(nodo->h2)){ // no preciso variable temporal
+    arg3 = get_numeric_node(nodo->h2);
+  } else {
+    decidir_nodo(nodo->h2, codigo); //ultima operacion debe ser numerica y guardar el resultado en arg1
+    arg3 = (RNumeric*)codigo->back()->arg1;
+  }
+
+  if (arg2->es_dec() || arg3->es_dec()){
+    RDecimal *d = new RDecimal();
+    arg1 = d;
+  }else{
+    RInteger *i = new RInteger();
+    arg1 = i;
+  }
+  codigo->push_back(instr(MULT, arg1, arg2, arg3));
+}
+
+void generar_plus(ast* nodo, std::list<Instruccion*> *codigo){ //suma de numericos
+  RNumeric* arg2, *arg3, *arg1;
+  if (nodo_hoja(nodo->h1)){ // no preciso variable temporal
+    arg2 = get_numeric_node(nodo->h1);
+  } else {
+    decidir_nodo(nodo->h1, codigo); //ultima operacion debe ser numerica y guardar el resultado en arg1
+    arg2 = (RNumeric*)codigo->back()->arg1;
+  }
+  if (nodo_hoja(nodo->h2)){ // no preciso variable temporal
+    arg3 = get_numeric_node(nodo->h2);
+  } else {
+    decidir_nodo(nodo->h2, codigo); //ultima operacion debe ser numerica y guardar el resultado en arg1
+    arg3 = (RNumeric*)codigo->back()->arg1;
+  }
+
+  if (arg2->es_dec() || arg3->es_dec()){
+    RDecimal *d = new RDecimal();
+    arg1 = d;
+  }else{
+    RInteger *i = new RInteger();
+    arg1 = i;
+  }
+  codigo->push_back(instr(ADD, arg1, arg2, arg3));
+}
+
+RNumeric* get_numeric_node(ast* hoja){
+  RNumeric* arg=NULL;
+  switch(hoja->tipo){
+    case f_entero:{
+      arg = new RInteger(hoja->entero);
+      break;}
+    case f_decimal:{
+      arg = new RDecimal(hoja->decimal);
+      break;}
+    default:
+      cout << "Error de tipo, el operando no es numerico!" << endl;
+      break;
+  }
+  return arg;
+}
+
+RNumeric* generar_div(ast* nodo, std::list<Instruccion*> *codigo){ return NULL; }
+RNumeric* generar_sub(ast* nodo, std::list<Instruccion*> *codigo){ return NULL; }
+RNumeric* generar_pow(ast* nodo, std::list<Instruccion*> *codigo){ return NULL; }
+RInteger* generar_mod(ast* nodo, std::list<Instruccion*> *codigo){ return NULL; }
+void generar_elsif(ast* nodo, std::list<Instruccion*> *codigo){}
+void generar_while(ast* nodo, std::list<Instruccion*> *codigo){}
+void generar_string(ast* nodo, std::list<Instruccion*> *codigo){}
+void generar_entero(ast* nodo, std::list<Instruccion*> *codigo){}
+void generar_decimal(ast* nodo, std::list<Instruccion*> *codigo){}
+void generar_bool(ast* nodo, std::list<Instruccion*> *codigo){}
+void generar_puts(ast* nodo, std::list<Instruccion*> *codigo){
+  RObject * arg1;
+  if (nodo_hoja(nodo->h1)){ // no preciso variable temporal
+    ast* hoja = nodo->h1;
+    switch(hoja->tipo){
+      case f_string:
+        cout << "hoja string" << endl;
+        arg1= new RString(hoja->str);
+        break;
+      case f_entero:
+        arg1= (new RInteger(hoja->entero))->to_s();
+        break;
+      case f_decimal:
+        arg1 = (new RDecimal(hoja->decimal))->to_s();
+        break;
+      case f_bool:
+        arg1 = (new RBool(hoja->booleano))->to_s();
+        break;
+    }
+  } else {
+    decidir_nodo(nodo->h1, codigo);
+    cout << "last op " << codigo->back()->op << endl;
+    arg1 = codigo->back()->arg1;
+  }
+  Instruccion* i = new Instruccion;
+  i->op = PUTS;
+  i->arg1 = arg1;
+  codigo->push_back(i);
+}
+
+bool nodo_hoja(ast* nodo){
+  return (nodo->tipo == f_string || nodo->tipo == f_entero || nodo->tipo == f_decimal || nodo->tipo == f_bool);
+}
+
+Instruccion* instr(enum code_ops op){
+  Instruccion* inst = new Instruccion;
+  inst->op = op;
+  return inst;
+}
+
+Instruccion* instr(enum code_ops op, RObject* arg1){
+  Instruccion* inst = instr(op);
+  inst->arg1=arg1;
+  return inst;
+}
+
+Instruccion* instr(enum code_ops op, RObject* arg1, RObject* arg2){
+  Instruccion* inst = instr(op, arg1);
+  inst->arg2=arg2;
+  return inst;
+}
+
+Instruccion* instr(enum code_ops op, RObject* arg1, RObject* arg2, RObject* arg3){
+  Instruccion* inst = instr(op, arg1, arg2);
+  inst->arg3=arg3;
+  return inst;
+}
+
+void generar_gets(list<Instruccion*>* codigo){
+  codigo->push_back(instr(GETS, new RString()));
+}
+
+void freeList(list<ast*> *stmt_list){
+  list<ast*>::iterator it;
+  for (it=stmt_list->begin(); it != stmt_list->end(); it++)
+    freeTree(*it);
+  delete stmt_list;
+}
+
+void freeTree(ast* tree){
+  if (tree == NULL)
+    return;
+  switch(tree->tipo){
+    case c_if :
+      break;
+    case op_mul :
+      break;
+    case op_plus :
+      freeTree(tree->h1);
+      freeTree(tree->h2);
+      delete tree;
+      break;
+    case op_div :
+      freeTree(tree->h1);
+      freeTree(tree->h2);
+      delete tree;
+      break;
+    case op_sub :
+      freeTree(tree->h1);
+      freeTree(tree->h2);
+      delete tree;
+      break;
+    case op_pow :
+      freeTree(tree->h1);
+      freeTree(tree->h2);
+      delete tree;
+      break;
+    case op_mod :
+      freeTree(tree->h1);
+      freeTree(tree->h2);
+      delete tree;
+      break;
+    case c_elsif :
+      break;
+    case c_while :
+      break;
+    case f_string :
+      delete tree->str;
+      delete tree;
+      break;
+    case f_entero :
+      delete tree;
+      break;
+    case f_decimal :
+      delete tree;
+      break;
+    case f_bool :
+      delete tree;
+      break;
+    case t_puts :
+      freeTree(tree->h1);
+      delete tree;
+      break;
+    case t_gets :
+      delete tree;
+      break;
+    case t_method_call:
+      delete tree->str;
+      delete tree;
+      break;
+    case t_command:
+      delete tree->str;
+      delete tree;
+      break;
+    case t_nil:
+      delete tree;
+      break;
+    case t_mul_string :
+      freeTree(tree->h1);
+      break;
+    case t_add_string :
+      freeTree(tree->h1);
+      freeTree(tree->h2);
+      break;
+    case b_and:
+      freeTree(tree->h1);
+      freeTree(tree->h2);
+      break;
+    case b_or:
+      freeTree(tree->h1);
+      freeTree(tree->h2);
+      break;
+    case b_not:
+      freeTree(tree->h1);
+      freeTree(tree->h2);
+      break;
+    case b_mayor:
+      freeTree(tree->h1);
+      freeTree(tree->h2);
+      break;
+    case b_mayor_igual:
+      freeTree(tree->h1);
+      freeTree(tree->h2);
+      break;
+    case b_menor:
+      freeTree(tree->h1);
+      freeTree(tree->h2);
+      break;
+    case b_menor_igual:
+      freeTree(tree->h1);
+      freeTree(tree->h2);
+      break;
+    case b_doble_igual:
+      freeTree(tree->h1);
+      freeTree(tree->h2);
+      break;
+    case b_not_igual:
+      freeTree(tree->h1);
+      freeTree(tree->h2);
+      break;
+    case b_is_bool:
+      freeTree(tree->h1);
+      freeTree(tree->h2);
+      break;
+    case t_compstmt : // creo q nunca entra aca
+      freeList(tree->stmt_list);
+      delete tree;
+      break;
+    }
+}
+
+void printList(list<ast*> *stmt_list){ list<ast*>::iterator it;
+  for (it=stmt_list->begin(); it != stmt_list->end(); it++)
+    printTree(*it);
+}
+
 void printTree(ast* tree){
   if (tree == NULL)
     return;
@@ -308,253 +583,4 @@ void printTree(ast* tree){
       printList(tree->stmt_list);
       break;
     }
-}
-
-void printList(list<ast*> *stmt_list){ list<ast*>::iterator it;
-  for (it=stmt_list->begin(); it != stmt_list->end(); it++)
-    printTree(*it);
-}
-
-void generar_if(ast* nodo, std::list<Instruccion*> *codigo){}
-RNumeric* generar_mul(ast* nodo, std::list<Instruccion*> *codigo){ return NULL; }
-RNumeric* generar_plus(ast* nodo, std::list<Instruccion*> *codigo){ //suma de numericos
-  RNumeric* arg2, *arg3, *arg1;
-  if (nodo_hoja(nodo->h1)){ // no preciso variable temporal
-    arg2 = get_numeric_node(nodo->h1);
-  } else {
-    decidir_nodo(nodo->h1, codigo); //ultima operacion debe ser numerica y guardar el resultado en arg1
-    arg2 = (RNumeric*)codigo->back()->arg1;
-  }
-  if (nodo_hoja(nodo->h2)){ // no preciso variable temporal
-    arg3 = get_numeric_node(nodo->h2);
-  } else {
-    decidir_nodo(nodo->h2, codigo); //ultima operacion debe ser numerica y guardar el resultado en arg1
-    arg3 = (RNumeric*)codigo->back()->arg1;
-  }
-
-  if (arg2->es_dec() || arg3->es_dec()){
-    RDecimal *d = new RDecimal();
-    arg1 = d;
-  }else{
-    RInteger *i = new RInteger();
-    arg1 = i;
-  }
-  codigo->push_back(instr(ADD, arg1, arg2, arg3));
-  
-  return NULL;
-}
-
-RNumeric* get_numeric_node(ast* hoja){
-  RNumeric* arg=NULL;
-  switch(hoja->tipo){
-    case f_entero:{
-      arg = new RInteger(hoja->entero);
-      break;}
-    case f_decimal:{
-      arg = new RDecimal(hoja->decimal);
-      break;}
-    default:
-      cout << "Error de tipo, el operando no es numerico!" << endl;
-      break;
-  }
-  return arg;
-}
-
-RNumeric* generar_div(ast* nodo, std::list<Instruccion*> *codigo){ return NULL; }
-RNumeric* generar_sub(ast* nodo, std::list<Instruccion*> *codigo){ return NULL; }
-RNumeric* generar_pow(ast* nodo, std::list<Instruccion*> *codigo){ return NULL; }
-RInteger* generar_mod(ast* nodo, std::list<Instruccion*> *codigo){ return NULL; }
-void generar_elsif(ast* nodo, std::list<Instruccion*> *codigo){}
-void generar_while(ast* nodo, std::list<Instruccion*> *codigo){}
-void generar_string(ast* nodo, std::list<Instruccion*> *codigo){}
-void generar_entero(ast* nodo, std::list<Instruccion*> *codigo){}
-void generar_decimal(ast* nodo, std::list<Instruccion*> *codigo){}
-void generar_bool(ast* nodo, std::list<Instruccion*> *codigo){}
-void generar_puts(ast* nodo, std::list<Instruccion*> *codigo){
-  RObject * arg1;
-  if (nodo_hoja(nodo->h1)){ // no preciso variable temporal
-    ast* hoja = nodo->h1;
-    switch(hoja->tipo){
-      case f_string:
-        cout << "hoja string" << endl;
-        arg1= new RString(hoja->str);
-        break;
-      case f_entero:
-        arg1= (new RInteger(hoja->entero))->to_s();
-        break;
-      case f_decimal:
-        arg1 = (new RDecimal(hoja->decimal))->to_s();
-        break;
-      case f_bool:
-        arg1 = (new RBool(hoja->booleano))->to_s();
-        break;
-    }
-  } else {
-    decidir_nodo(nodo->h1, codigo);
-    cout << "last op " << codigo->back()->op << endl;
-    arg1 = codigo->back()->arg1;
-  }
-  Instruccion* i = new Instruccion;
-  i->op = PUTS;
-  i->arg1 = arg1;
-  codigo->push_back(i);
-}
-
-bool nodo_hoja(ast* nodo){
-  return (nodo->tipo == f_string || nodo->tipo == f_entero || nodo->tipo == f_decimal || nodo->tipo == f_bool);
-}
-
-void freeList(list<ast*> *stmt_list){
-  list<ast*>::iterator it;
-  for (it=stmt_list->begin(); it != stmt_list->end(); it++)
-    freeTree(*it);
-  delete stmt_list;
-}
-
-void freeTree(ast* tree){
-  if (tree == NULL)
-    return;
-  switch(tree->tipo){
-    case c_if :
-      break;
-    case op_mul :
-      break;
-    case op_plus :
-      freeTree(tree->h1);
-      freeTree(tree->h2);
-      delete tree;
-      break;
-    case op_div :
-      freeTree(tree->h1);
-      freeTree(tree->h2);
-      delete tree;
-      break;
-    case op_sub :
-      freeTree(tree->h1);
-      freeTree(tree->h2);
-      delete tree;
-      break;
-    case op_pow :
-      freeTree(tree->h1);
-      freeTree(tree->h2);
-      delete tree;
-      break;
-    case op_mod :
-      freeTree(tree->h1);
-      freeTree(tree->h2);
-      delete tree;
-      break;
-    case c_elsif :
-      break;
-    case c_while :
-      break;
-    case f_string :
-      delete tree->str;
-      delete tree;
-      break;
-    case f_entero :
-      delete tree;
-      break;
-    case f_decimal :
-      delete tree;
-      break;
-    case f_bool :
-      delete tree;
-      break;
-    case t_puts :
-      freeTree(tree->h1);
-      delete tree;
-      break;
-    case t_gets :
-      delete tree;
-      break;
-    case t_method_call:
-      delete tree->str;
-      delete tree;
-      break;
-    case t_command:
-      delete tree->str;
-      delete tree;
-      break;
-    case t_nil:
-      delete tree;
-      break;
-    case t_mul_string :
-      freeTree(tree->h1);
-      break;
-    case t_add_string :
-      freeTree(tree->h1);
-      freeTree(tree->h2);
-      break;
-    case b_and:
-      freeTree(tree->h1);
-      freeTree(tree->h2);
-      break;
-    case b_or:
-      freeTree(tree->h1);
-      freeTree(tree->h2);
-      break;
-    case b_not:
-      freeTree(tree->h1);
-      freeTree(tree->h2);
-      break;
-    case b_mayor:
-      freeTree(tree->h1);
-      freeTree(tree->h2);
-      break;
-    case b_mayor_igual:
-      freeTree(tree->h1);
-      freeTree(tree->h2);
-      break;
-    case b_menor:
-      freeTree(tree->h1);
-      freeTree(tree->h2);
-      break;
-    case b_menor_igual:
-      freeTree(tree->h1);
-      freeTree(tree->h2);
-      break;
-    case b_doble_igual:
-      freeTree(tree->h1);
-      freeTree(tree->h2);
-      break;
-    case b_not_igual:
-      freeTree(tree->h1);
-      freeTree(tree->h2);
-      break;
-    case b_is_bool:
-      freeTree(tree->h1);
-      freeTree(tree->h2);
-      break;
-    case t_compstmt : // creo q nunca entra aca
-      freeList(tree->stmt_list);
-      delete tree;
-      break;
-    }
-}
-
-Instruccion* instr(enum code_ops op){
-  Instruccion* inst = new Instruccion;
-  inst->op = op;
-  return inst;
-}
-
-Instruccion* instr(enum code_ops op, RObject* arg1){
-  Instruccion* inst = instr(op);
-  inst->arg1=arg1;
-  return inst;
-}
-Instruccion* instr(enum code_ops op, RObject* arg1, RObject* arg2){
-  Instruccion* inst = instr(op, arg1);
-  inst->arg2=arg2;
-  return inst;
-}
-Instruccion* instr(enum code_ops op, RObject* arg1, RObject* arg2, RObject* arg3){
-  Instruccion* inst = instr(op, arg1, arg2);
-  inst->arg3=arg3;
-  return inst;
-}
-
-void generar_gets(list<Instruccion*>* codigo){
-  codigo->push_back(instr(GETS, new RString()));
 }
